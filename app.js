20862e022c677a3d02bc790c70fe6c8afd339f5d
kasireddyKirthanareddy <kirthana0808reddy@gmail.com>
2023-11-07 16:15:07 +0530

    committing todo project

---
+++ app.js
@@ -0,0 +1,14 @@
+var addBtn=document.getElementById("add-task")
+var taskInput=document.getElementById("task")
+var tasklist=document.getElementById("tasks")
+
+addBtn.addEventListener("click",function(){
+    var task=taskInput.value 
+    addTask(task)
+    taskInput.value = ""
+})
+function addTask(task){
+    var li= document.createElement("li")
+    li.innerHTML=task
+    tasklist.appendChild(li)
+}
